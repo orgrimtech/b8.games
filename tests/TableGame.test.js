94c091c13875937b960d44b0c12cdcd57fdfb242
Ben Sumer <adam.sumer.hh@gmail.com>
2022-11-20 19:44:25 -0800

    table game deployment test.

--- tests/TableGame.test.js
+++ tests/TableGame.test.js
@@ -3,11 +3,13 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("TableGame", function () {
-  it("test initial value", async function () {
-    const TableGame = await ethers.getContractFactory("TableGame");
-    const table = await TableGame.deploy();
-    await table.deployed();
-    console.log('storage deployed at:'+ table.address)
-    expect((await table.getAccumulatedBalance()).toNumber()).to.equal(10);
+  describe("- Deployment", function () {
+    it("test initial value", async function () {
+      const TableGame = await ethers.getContractFactory("TableGame");
+      const table = await TableGame.deploy();
+      await table.deployed();
+      console.log('TableGame deployed at:'+ table.address)
+      expect((await table.getAccumulatedBalance()).toNumber()).to.equal(0);
+    });
   });
 });
